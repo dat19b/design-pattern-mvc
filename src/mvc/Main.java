d2d25adfbd142902000885b61f1907b9602c1b48
clbokea <clbo@kea.dk>
2020-11-05 09:23:56 +0100

    hhh

--- src/mvc/Main.java
+++ src/mvc/Main.java
@@ -1,8 +1,30 @@
 package mvc;
 
+import mvc.controller.StudentController;
+import mvc.model.Student;
+import mvc.view.StudentView;
+
 public class Main {
 
     public static void main(String[] args) {
+        Student model = retriveStudentFromDatabase();
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(model, view);
+
+        // eventloop
+        controller.updateView();
+        controller.setStudentName("Claus");
+        controller.updateView();
 
     }
+
+
+    private static Student retriveStudentFromDatabase(){
+        Student student = new Student();
+        student.setName("Robert");
+        student.setRollNo("10");
+        return student;
+    }
+
+
 }
